ef60d76f3acb5297685f7eb609adb6aa066d8227
ikm216 <ikmclassof22@gmail.com>
2024-11-23 20:58:30 -0500

    doc: added documentation to the feature covertToFah function

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 def convertToFah(cel):
+    """
+    Convert celsius to fahrenheit
+
+    Args:
+        cel: The celsius that being converted
+
+    Returns:
+        The final result of celsius being converted to fahrenheit
+    """
     return (cel * 9/5) + 32
 
 def test_should_return_89point6_from_32():
     assert convertToFah(32) == 89.6
-    
