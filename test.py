76686b6a84c1b840b2d6b2ffd4f0fe450099b23f
ikm216 <ikmclassof22@gmail.com>
2024-11-23 21:04:13 -0500

    feat: added covertToCel function to convert Fahrenheit to Celsius

--- test.py
+++ test.py
@@ -10,6 +10,9 @@ def convertToFah(cel):
     """
     return (cel * 9/5) + 32
 
+def convertToCel(fah):
+    return (fah - 32) * 5/9
+
 def test_should_return_89point6_from_32():
     assert convertToFah(32) == 89.6
 
